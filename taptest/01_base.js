b237fcc6d06c49d92cc1ef86bb037dee2ad72b36
Jxck <block.rxckin.beats@gmail.com>
2010-10-24 17:56:50 +0900

    add JSTAPd for test

---
+++ taptest/01_base.js
@@ -0,0 +1,6 @@
+ok($, 'ok $');
+ok(bbs, 'ok bbs');
+is('test', 'test', 'is');
+isnt('test', 'dev', 'isnt');
+like('test', new RegExp('es'), 'like');
+is(tap$('test').innerHTML, 'DATA', 'getElementById');
